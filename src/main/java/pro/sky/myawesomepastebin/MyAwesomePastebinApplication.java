66b9df8aa06e7b2f0fedb2d9bf1678ce61d5b182
Руслан <rezwalqer@gmail.com>
2023-04-13 13:11:10 +0500

    Initial commit

---
+++ src/main/java/pro/sky/myawesomepastebin/MyAwesomePastebinApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.myawesomepastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwesomePastebinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAwesomePastebinApplication.class, args);
+    }
+
+}
